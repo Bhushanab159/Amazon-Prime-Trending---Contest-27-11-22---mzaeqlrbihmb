f71d8f1e268557c520623098db0a3ec2a11f2b54
Shraddha08-cmyk <shraddhatiwari8175@gmail.com>
2022-11-27 16:40:46 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,61 @@
-import React, {Component, useState} from "react";
-import { ProgressPlugin } from "webpack";
+import React, {useState, useEffect} from "react";
 import '../styles/App.css';
 
-const App = (props) => {
-  const [counter, setCounter] = useState(0);
-  const [data] = useState(props.slides);
-  
+const App = ({slides}) => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [prevFlag, setPrevFlag] = useState(true);
+  const [nextFlag, setNextFlag] = useState(false);
+  const [restartFlag, setRestartFlag] = useState(true);
+
+  useEffect(() => {
+    // if current slide is the first one
+    if(slideIndex === 0) {
+      setPrevFlag(true);
+      setRestartFlag(true);
+    } else {
+      setPrevFlag(false);
+      setRestartFlag(false);
+    }
+
+    // if current slide is the last one
+    if(slideIndex === slides.length - 1) {
+      setNextFlag(true);
+    } else {
+      setNextFlag(false);
+    }
+
+  }, [slideIndex])
+
+  const handlePrevClick = () => {
+      
+      setSlideIndex((prevState) => prevState - 1);
+  }
+
+  const handleNextClick = () => {
+      setSlideIndex((prevState) => prevState + 1);
+  }
+
+  const handleRestartClick = () => {
+    setSlideIndex(0);
+  }
+
+
+
   return (
-    <div>
-      <h1 data-testid="title">{data[counter].title}</h1>
-      <p data-testid="text">{data[counter].text}</p>
-      <button data-testid="button-next" disabled={counter === props.slides.length-1 ? true : false} onclick={() => setCounter(counter + 1)}>Next</button>
-      <button data-testid="button-prev" disabled={counter === 0 ? true : false} onclick={() => setCounter(counter - 1)}>Prev</button>
-      <button data-testid="button-restart" disabled={counter === 0 ? true : false} onclick={() => setCounter(0)}>Restart</button>
-    </div>
+    <>
+      <h1 id="heading">Amazon Prime: Trending</h1>
+      <div>
+        <div>
+          <h1 data-testid="title">{slides[slideIndex].title}</h1>
+          <p data-testid="text">{slides[slideIndex].text}</p>
+        </div>
+        
+        <button data-testid="button-prev" onClick={handlePrevClick} disabled={prevFlag}>Prev</button>
+        <button data-testid="button-next" onClick={handleNextClick} disabled={nextFlag}>Next</button>
+        <button data-testid="button-restart" onClick={handleRestartClick} disabled={restartFlag}>Restart</button>
+    
+      </div>
+    </>
   )
 }
 
